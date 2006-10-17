f941099e1094707a6fa74974394b3d1fa2d22f08
slavianp <>
2006-10-17 10:32:13 +0000

    edited from eclipse

--- test_module/com/improc/TestClass.java
+++ test_module/com/improc/TestClass.java
@@ -2,6 +2,9 @@ package com.improc;
 
 public class TestClass {
 
+	/**
+	 * Some comment
+	 */
 	public static void main(String[] args) {
 		// Edited again with notepad
 		System.out.println("I already made some changes in the repository");	
