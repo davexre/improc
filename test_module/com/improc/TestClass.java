67dabac21a8d0aaf3f6c9c98d79b79fcac59886d
slavianp <>
2006-10-17 10:33:43 +0000

    comit form notepad

--- test_module/com/improc/TestClass.java
+++ test_module/com/improc/TestClass.java
@@ -6,10 +6,6 @@ public class TestClass {
 	 * Some comment
 	 */
 	public static void main(String[] args) {
-		// Edited again with notepad
 		System.out.println("I already made some changes in the repository");	
-		// Added from notepad
-		// Edited in eclipse
-		// More added
 	}
 }
