3d0bdbbb76fd3a7fae1676a4b0eabb62e4da9a14
slavian <slavian@slavian>
2016-12-18 23:04:20 +0200

    logback sample simplified

--- example/example.webapp.logback/src/main/java/example/webapp/logback/ApplicationListener.java
+++ example/example.webapp.logback/src/main/java/example/webapp/logback/ApplicationListener.java
@@ -1,6 +1,5 @@
 package example.webapp.logback;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.servlet.ServletContext;
@@ -13,64 +12,33 @@ import org.slf4j.LoggerFactory;
 
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
-import ch.qos.logback.core.joran.spi.JoranException;
 
 @WebListener
 public class ApplicationListener implements ServletContextListener {
-	static Object synch = new Object();
-	static ServletContext servletContext = null;
-	static LoggerContext loggerContext = null;
-	static boolean loggerInitialized = false;
-	
 	static final Logger log = LoggerFactory.getLogger(ApplicationListener.class);
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		try {
+			ServletContext servletContext = sce.getServletContext();
+			String tempDir = String.valueOf(servletContext.getAttribute(ServletContext.TEMPDIR));
+			URL url = servletContext.getResource("/WEB-INF/logback.xml");
+			LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+			loggerContext.reset();
+			loggerContext.putProperty("tempDir", tempDir);
+
+			JoranConfigurator configurator = new JoranConfigurator();
+			configurator.setContext(loggerContext);
+			configurator.doConfigure(url);
+			log.info("Log output dir is " + tempDir);
+		} catch (Exception e) {
+			log.error("Error configuring logback", e);
+		}
 		JUL.initialise();
 		SystemOut.initialise();
-		synchronized (synch) {
-			servletContext = sce.getServletContext();
-			updateLogger();
-		}
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		synchronized (synch) {
-			servletContext = null;
-		}
-	}
-	
-	public static ServletContext getServletContext() {
-		synchronized (synch) {
-			return servletContext;
-		}
-	}
-
-	public static void setLoggerContext(LoggerContext loggerContext) {
-		synchronized (synch) {
-			ApplicationListener.loggerContext = loggerContext;
-			updateLogger();
-		}
-	}
-	
-	private static void updateLogger() {
-		synchronized (synch) {
-			if (loggerInitialized || servletContext == null || loggerContext == null) {
-				return;
-			}
-			try {
-				String tempDir = String.valueOf(servletContext.getAttribute(ServletContext.TEMPDIR));
-				URL url = servletContext.getResource("/WEB-INF/logback.xml");
-				JoranConfigurator configurator = new JoranConfigurator();
-				loggerContext.putProperty("tempDir", tempDir);
-				configurator.setContext(loggerContext);
-				configurator.doConfigure(url);
-				loggerInitialized = true;
-				log.info("Log output dir is " + tempDir);
-			} catch (MalformedURLException | JoranException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 }
