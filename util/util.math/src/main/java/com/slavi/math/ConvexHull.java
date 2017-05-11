60709b6ea6459af52f914b4e7711c31adef7573d
Slavian Petrov <slavianp@gmail.com>
2017-05-12 00:19:42 +0300

    .

--- util/util.math/src/main/java/com/slavi/math/ConvexHull.java
+++ util/util.math/src/main/java/com/slavi/math/ConvexHull.java
@@ -2,7 +2,10 @@ package com.slavi.math;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 public class ConvexHull {
 
@@ -10,38 +13,104 @@ public class ConvexHull {
 	 * See
 	 * http://en.wikipedia.org/wiki/Polygon_area 
 	 */
-	public static double getPolygonArea(Iterator<Point2D.Double> polygon) {
+	public static <T extends Point2D> double getPolygonArea(Iterator<T> polygon) {
 		if (polygon == null || (!polygon.hasNext()))
 			return 0.0;
 		double result = 0.0;
-		Point2D.Double firstPoint, curPoint;
+		T firstPoint, curPoint;
 		firstPoint = curPoint = polygon.next();
 		while (polygon.hasNext()) {
-			Point2D.Double nextPoint = polygon.next();
-			result += curPoint.x * nextPoint.y - nextPoint.x * curPoint.y;
+			T nextPoint = polygon.next();
+			result += curPoint.getX() * nextPoint.getY() - nextPoint.getX() * curPoint.getY();
 			curPoint = nextPoint;
 		}
-		result += curPoint.x * firstPoint.y - firstPoint.x * curPoint.y;
+		result += curPoint.getX() * firstPoint.getY() - firstPoint.getX() * curPoint.getY();
 		return result * 0.5;
 	}
 	
-	public static double getPolygonArea(Iterable<Point2D.Double> polygon) {
+	public static <T extends Point2D> double getPolygonArea(Iterable<T> polygon) {
 		return getPolygonArea(polygon.iterator());
+	}
+	
+	private static class ComparePoints<T extends Point2D> implements Comparator<T> {
+		public T origin;
+		
+		// -1 left turn or p2 is closer to origin; 0 equal; 1 right turn 
+		public int compare(T p2, T p3) {
+			double dx2 = p2.getX() - origin.getX();
+			double dy2 = p2.getY() - origin.getY();
+			double dx3 = p3.getX() - origin.getX();
+			double dy3 = p3.getY() - origin.getY();
+			int result = Double.compare(dy2 * dx3, dx2 * dy3);
+			if (result == 0) {
+				return Double.compare(
+						Math.abs(dx2) + Math.abs(dy2), 
+						Math.abs(dx3) + Math.abs(dy3));
+			}
+			return result;
+		}
+	}
+	
+	/**
+	 * The list of points is reoreded!
+	 * https://en.wikipedia.org/wiki/Graham_scan
+	 */
+	public static <T extends Point2D> ArrayList<T> makeConvexHull2(List<T> points) {
+		ArrayList<T> result = new ArrayList<>();
+		if (points.size() < 1)
+			return result;
+		
+		ComparePoints<T> comparePoints = new ComparePoints<>();
+		comparePoints.origin = points.get(0);
+		for (int i = points.size() - 1; i >= 1; i--) {
+			T p = points.get(i);
+			if ((p.getY() < comparePoints.origin.getY()) ||
+				((p.getY() == comparePoints.origin.getY()) && (p.getX() < comparePoints.origin.getX()))) {
+				comparePoints.origin = p;
+			}
+		}
+		Collections.sort(points, comparePoints);
+
+		result.add(comparePoints.origin);
+		for (T p : points) {
+			while (true) {
+				int size = result.size();
+				T p2 = null;
+				if (size > 0) {
+					p2 = result.get(size - 1);
+					if (p2.getX() == p.getX() && p2.getY() == p.getY())
+						break; // equal points
+				}
+				if (size < 2) {
+					result.add(p);
+					break;
+				}
+				comparePoints.origin = result.get(size - 2);
+				int r = comparePoints.compare(p2, p);
+				if (r < 0) {
+					// left turn
+					result.add(p);
+					break;
+				}
+				result.remove(size - 1);
+			}
+		}
+		return result;
 	}
 	
 	/**
 	 * Ideas borrowed from
 	 * http://www.dr-mikes-maths.com/DotPlacer.html 
 	 */
-	public static ArrayList<Point2D.Double> makeConvexHull(Iterable<Point2D.Double> points) {
-		ArrayList<Point2D.Double> result = new ArrayList<Point2D.Double>();
+	public static <T extends Point2D> ArrayList<T> makeConvexHull(Iterable<T> points) {
+		ArrayList<T> result = new ArrayList<>();
 		
 		// Find the left-most point, i.e. min X
-		Point2D.Double startPoint = null;
+		T startPoint = null;
 		double minX = Double.MAX_VALUE;
-		for (Point2D.Double point : points) {
-			if (point.x < minX) {
-				minX = point.x;
+		for (T point : points) {
+			if (point.getX() < minX) {
+				minX = point.getX();
 				startPoint = point;
 			}
 		}
@@ -49,16 +118,16 @@ public class ConvexHull {
 			return result;
 		result.add(startPoint);
 		
-		Point2D.Double currentPoint = startPoint;
+		T currentPoint = startPoint;
 		double localX = 1.0;
 		double localY = 0.0;
 		while (true) {
-			Point2D.Double nextPoint = null;
+			T nextPoint = null;
 			double angle = -2.0;
 			double length = -1;
-			for (Point2D.Double point : points) {
-				double dX = point.x - currentPoint.x;
-				double dY = point.y - currentPoint.y;
+			for (T point : points) {
+				double dX = point.getX() - currentPoint.getX();
+				double dY = point.getY() - currentPoint.getY();
 				double hypot = MathUtil.hypot(dX, dY);
 				if (hypot != 0) {
 					double cur = (localX * dX + localY * dY) / hypot;
@@ -72,8 +141,8 @@ public class ConvexHull {
 			if (nextPoint == null)
 				return result;
 			result.add(nextPoint);
-			double dX = nextPoint.x - currentPoint.x;
-			double dY = nextPoint.y - currentPoint.y;
+			double dX = nextPoint.getX() - currentPoint.getX();
+			double dY = nextPoint.getY() - currentPoint.getY();
 			double hypot = MathUtil.hypot(dX, dY);
 			if (hypot != 0) {
 				localX = dX / hypot;
@@ -81,8 +150,8 @@ public class ConvexHull {
 			}
 			currentPoint = nextPoint;
 
-			dX = nextPoint.x - startPoint.x;
-			dY = nextPoint.y - startPoint.y;
+			dX = nextPoint.getX() - startPoint.getX();
+			dY = nextPoint.getY() - startPoint.getY();
 			if ((dX == 0.0) && (dY == 0.0))
 				break;
 		}
@@ -93,30 +162,30 @@ public class ConvexHull {
 	 * Ideas borrowed from
 	 * http://www.dr-mikes-maths.com/DotPlacer.html 
 	 */
-	public static double getConvexHullArea(Iterable<Point2D.Double> points) {
+	public static <T extends Point2D> double getConvexHullArea(Iterable<T> points) {
 		double result = 0.0;
 		
 		// Find the left-most point, i.e. min X
-		Point2D.Double startPoint = null;
+		T startPoint = null;
 		double minX = Double.MAX_VALUE;
-		for (Point2D.Double point : points) {
-			if (point.x < minX) {
-				minX = point.x;
+		for (T point : points) {
+			if (point.getX() < minX) {
+				minX = point.getX();
 				startPoint = point;
 			}
 		}
 		if (startPoint == null)
 			return 0.0;
-		Point2D.Double currentPoint = startPoint;
+		T currentPoint = startPoint;
 		double localX = 1.0;
 		double localY = 0.0;
 		while (true) {
-			Point2D.Double nextPoint = null;
+			T nextPoint = null;
 			double angle = -2.0;
 			double length = -1;
-			for (Point2D.Double point : points) {
-				double dX = point.x - currentPoint.x;
-				double dY = point.y - currentPoint.y;
+			for (T point : points) {
+				double dX = point.getX() - currentPoint.getX();
+				double dY = point.getY() - currentPoint.getY();
 				double hypot = MathUtil.hypot(dX, dY);
 				if (hypot != 0) {
 					double cur = (localX * dX + localY * dY) / hypot;
@@ -129,9 +198,9 @@ public class ConvexHull {
 			}
 			if (nextPoint == null)
 				return 0.0;
-			result += currentPoint.x * nextPoint.y - nextPoint.x * currentPoint.y;
-			double dX = nextPoint.x - currentPoint.x;
-			double dY = nextPoint.y - currentPoint.y;
+			result += currentPoint.getX() * nextPoint.getY() - nextPoint.getX() * currentPoint.getY();
+			double dX = nextPoint.getX() - currentPoint.getX();
+			double dY = nextPoint.getY() - currentPoint.getY();
 			double hypot = MathUtil.hypot(dX, dY);
 			if (hypot != 0) {
 				localX = dX / hypot;
@@ -139,19 +208,19 @@ public class ConvexHull {
 			}
 			currentPoint = nextPoint;
 
-			dX = nextPoint.x - startPoint.x;
-			dY = nextPoint.y - startPoint.y;
+			dX = nextPoint.getX() - startPoint.getX();
+			dY = nextPoint.getY() - startPoint.getY();
 			if ((dX == 0.0) && (dY == 0.0))
 				break;
 		}
 		return result * 0.5;
 	}
 
-	public static class ConvexHullArea extends AbstractConvexHullArea {
-		ArrayList<Point2D.Double> points;
+	public static class ConvexHullArea<T extends Point2D> extends AbstractConvexHullArea {
+		ArrayList<T> points;
 		int curPoint;
 		
-		public ConvexHullArea(ArrayList<Point2D.Double> points) {
+		public ConvexHullArea(ArrayList<T> points) {
 			this.points = points;
 			curPoint = -1;
 		}
@@ -165,11 +234,11 @@ public class ConvexHull {
 		}
 
 		public double getX() {
-			return points.get(curPoint).x;
+			return points.get(curPoint).getX();
 		}
 
 		public double getY() {
-			return points.get(curPoint).y;
+			return points.get(curPoint).getY();
 		}
 	}
 }
--- work/src/main/java/com/kattis/solution/Solution3.java
+++ work/src/main/java/com/kattis/solution/Solution3.java
@@ -2,9 +2,9 @@ package com.kattis.solution;
 
 import java.io.BufferedInputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
-
-import com.slavi.math.MathUtil;
 
 public class Solution3 {
 
@@ -21,6 +21,24 @@ public class Solution3 {
 		}
 	}
 
+	Point origin;
+	// -1 left turn or p2 is closer to origin; 0 equal; 1 right turn 
+	Comparator comparePoints = new Comparator<Point>() {
+		public int compare(Point p2, Point p3) {
+			int dx2 = p2.x - origin.x;
+			int dy2 = p2.y - origin.y;
+			int dx3 = p3.x - origin.x;
+			int dy3 = p3.y - origin.y;
+			int result = Integer.compare(dy2 * dx3, dx2 * dy3);
+			if (result == 0) {
+				return Integer.compare(
+						Math.abs(dx2) + Math.abs(dy2), 
+						Math.abs(dx3) + Math.abs(dy3));
+			}
+			return result;
+		}
+	};
+	
 	public void doIt(Scanner scanner) throws Exception {
 		while (scanner.hasNext()) {
 			int numberOfPoints = scanner.nextInt();
@@ -34,61 +52,48 @@ public class Solution3 {
 			Point startPoint = points.get(0);
 			for (int i = 1; i < numberOfPoints; i++) {
 				Point p = points.get(i);
-				if ((p.x < startPoint.x) ||
-					((p.x == startPoint.x) && (p.y > startPoint.y))) {
+				if ((p.y < startPoint.y) ||
+					((p.y == startPoint.y) && (p.x < startPoint.x))) {
 					startPoint = p;
 				}
 			}
-			//ArrayList<Point> convex = new ArrayList<>();
-			//convex.add(startPoint);
-			Point currentPoint = startPoint;
-			double localX = 1.0;
-			double localY = 0.0;
-			double area = 0;
-			while (true) {
-				Point point;
-				Point nextPoint = null;
-				double angle = -2;
-				double length = -1;
-				for (int i = 0; i < numberOfPoints; i++) {
-					point = points.get(i);
-					double dX = point.x - currentPoint.x;
-					double dY = point.y - currentPoint.y;
-					double hypot = Math.sqrt(dX*dX + dY*dY);
-					if (hypot <= 0.0)
-						continue;
-					double cur = (localX * dX + localY * dY) / hypot;
-					if ((cur > angle) || ((cur == angle) && (hypot > length))) {
-						nextPoint = point;
-						length = hypot;
-						angle = cur;
-					}
-				}
-				if (nextPoint == null)
-					break;
-				//convex.add(nextPoint);
-				double dX = nextPoint.x - currentPoint.x;
-				double dY = nextPoint.y - currentPoint.y;
-				double hypot = MathUtil.hypot(dX, dY);
-				if (hypot != 0) {
-					localX = dX / hypot;
-					localY = dY / hypot;
-				}
-				area += nextPoint.x * currentPoint.y - currentPoint.x * nextPoint.y;
-				currentPoint = nextPoint;
+			origin = startPoint;
+			//points.remove(origin);
+			Collections.sort(points, comparePoints);
 
-				dX = nextPoint.x - startPoint.x;
-				dY = nextPoint.y - startPoint.y;
-				if ((dX == 0.0) && (dY == 0.0))
-					break;
+			ArrayList<Point> convex = new ArrayList<>();
+			convex.add(origin);
+			for (Point p : points) {
+				while (true) {
+					int size = convex.size();
+					if (size < 2) {
+						convex.add(p);
+						break;
+					}
+					origin = convex.get(size - 2);
+					Point p2 = convex.get(size - 1);
+					int r = comparePoints.compare(p2, p);
+					if (r < 0) {
+						// left turn
+						convex.add(p);
+						break;
+					}
+					convex.remove(size - 1);
+				}
 			}
-			area /= 2.0;
-			System.out.println(String.format("%.1f", area));
+			
+			Point currentPoint = convex.get(convex.size() - 1);
+			int area = 0;
+			for (Point nextPoint : convex) {
+				area += currentPoint.x * nextPoint.y - nextPoint.x * currentPoint.y;
+				currentPoint = nextPoint;
+			}
+			System.out.println(String.format("%.1f", area * 0.5));
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		new Solution3().doIt(new Scanner(new BufferedInputStream(Solution1.class.getResourceAsStream("Solution3-data.txt"))));
-		//new Solution3().doIt(new Scanner(new BufferedInputStream(System.in)));
+		//new Solution3().doIt(new Scanner(new BufferedInputStream(Solution1.class.getResourceAsStream("Solution3-data.txt"))));
+		new Solution3().doIt(new Scanner(new BufferedInputStream(System.in)));
 	}
 }
