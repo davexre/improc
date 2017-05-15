d651cb3699959ef2db86f10c36bacb618cbf59c9
Slavian Petrov <slavianp@gmail.com>
2017-05-16 00:13:29 +0300

    .

--- example/spring.boot.itest/src/test/java/com/slavi/example/springBoot/example2/itest/Test1.java
+++ example/spring.boot.itest/src/test/java/com/slavi/example/springBoot/example2/itest/Test1.java
@@ -1,17 +1,13 @@
 package com.slavi.example.springBoot.example2.itest;
 
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.StrSubstitutor;
+import org.junit.Before;
 import org.junit.BeforeClass;
-import org.springframework.context.expression.MapAccessor;
-import org.springframework.expression.common.TemplateParserContext;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.junit.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.authentication.FormAuthConfig;
@@ -21,28 +17,46 @@ import io.restassured.specification.RequestSpecification;
 
 public class Test1 extends RestAssured {
 
+	protected static StrSubstitutor subst;
+	
 	@BeforeClass
 	public static void beforeClass() throws Exception {
 		Properties prop = new Properties();
 		prop.load(new InputStreamReader(Test1.class.getResourceAsStream("/test.properties")));
-
-		RestAssured.port = 8080;
-		RestAssured.basePath = "/";
-		RestAssured.baseURI = "http://localhost";
+		prop.putAll(prop);
+		prop.putAll(System.getenv());
+		prop.putAll(System.getProperties());
+		subst = new StrSubstitutor((Map) prop);
+		subst.setEnableSubstitutionInVariables(true);
+		
+		RestAssured.port = Integer.parseInt(subst.replace("${server.port}"));
+		RestAssured.basePath = subst.replace("${server.basePath}");
+		RestAssured.baseURI = subst.replace("${server.baseURI}");
 	}
 
-	void doIt() throws Exception {
+	RequestSpecification authorized;
+	@Before
+	public void prepare() throws Exception {
 		SessionFilter sessionFilter = new SessionFilter();
-		RequestSpecification rs = given().filter(sessionFilter)
-				.auth().form("admin", "admin", new FormAuthConfig("/login", "username", "password").withCsrfFieldName("_csrf"));
-		Response r = rs.get("/users");
+		authorized = given().filter(sessionFilter).auth()
+			.form(
+				subst.replace("{login.admin.user}"), 
+				subst.replace("{login.admin.pass}"), 
+				new FormAuthConfig("/login", "username", "password").withCsrfFieldName("_csrf")
+			);
+	}
+
+	@Test
+	public void doIt() throws Exception {
+		Response r = authorized.get("/users");
 		r.then().statusCode(200);
 		System.out.println(r.getBody().asString());
 
 	}
 
 	public static void main(String[] args) throws Exception {
-		Properties prop = new Properties();
+		beforeClass();
+/*		Properties prop = new Properties();
 		prop.load(new InputStreamReader(Test1.class.getResourceAsStream("/test.properties")));
 		Map map = new HashMap(prop);
 
@@ -57,9 +71,10 @@ public class Test1 extends RestAssured {
 		Object o = parser.parseExpression(spel, templateContext).getValue(context);
 		System.out.println(o);
 
-		StrSubstitutor subst = new StrSubstitutor(map);
+		StrSubstitutor subst = new StrSubstitutor(prop);
 		System.out.println(subst.replace("asd ${test.substitute} qwe"));
-
+*/
+		new Test1().doIt();
 		System.out.println("Done.");
 	}
 }
