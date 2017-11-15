99664b6ce861e6531ee4f14182244dfeccff60bd
Slavian Petrov <slavianp@gmail.com>
2017-11-15 23:37:01 +0100

    .

--- work/src/main/java/com/slavi/ann/test/MyLayer.java
+++ work/src/main/java/com/slavi/ann/test/MyLayer.java
@@ -74,10 +74,28 @@ public class MyLayer {
 	}
 
 	public void eraseWeights_2() {
-		fillWeight(weight, 0.5);
+		fillWeight(weight, 0.3);
 	}
 
+	static double sqrt2pi = Math.sqrt(2.0 * Math.PI);
+	
 	public static void fillWeight(Matrix w, double stdDev) {
+		double scale = 1.0 / (2 * stdDev * stdDev); // * w.getSizeX());
+		double scale2 = 1.0 / (stdDev * sqrt2pi);
+		double w2 = w.getSizeX() / 2.0;
+		for (int j = w.getSizeY() - 1; j >= 0; j--) {
+			double tr = w2 - j * w.getSizeX() / w.getSizeY();
+			for (int i = w.getSizeX() - 1; i >= 0; i--) {
+				double d = (i + tr) % w.getSizeX();
+				if (d < 0)
+					d += w.getSizeX();
+				d -= w2;
+				w.setItem(i, j, Math.exp(-d*d*scale) * scale2);
+			}
+		}
+	}
+
+	public static void fillWeight_quick(Matrix w, double stdDev) {
 		double scale = 1.0 / (stdDev * w.getSizeX());
 		double w2 = w.getSizeX() / 2.0;
 		for (int j = w.getSizeY() - 1; j >= 0; j--) {
