ecd34eddd9d4e8236fac2010abd8e929728ecf77
Slavian Petrov <slavianp@gmail.com>
2018-04-17 23:10:53 +0300

    .

--- work/src/main/java/com/test/swing/DummyUi.java
+++ work/src/main/java/com/test/swing/DummyUi.java
@@ -1,7 +1,9 @@
 package com.test.swing;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
@@ -17,58 +19,60 @@ public class DummyUi {
 
 	
 	public static class SwingMatrix extends JComponent {
-		Object lock = new Object();
-		Matrix m = new Matrix();
+		int baseColor = 0x123456;
+		BufferedImage img;
 		
 		public SwingMatrix() {
-			setDoubleBuffered(true);
+			setOpaque(true);
+			//setDoubleBuffered(true);
 		}
 		
 		public synchronized void setValue(Matrix value) {
-			if (value == null) {
-				m.resize(0, 0);
-			} else {
-				value.copyTo(m);
-			}
+			img = genImg(img, value, baseColor);
 			repaint();
 		}
 		
-		int baseColor = 0x123456;
-		public Color getColor(double value) {
-			double col[] = new double[3];
-			ColorConversion.RGB.fromRGB(baseColor, col);
-			ColorConversion.HSL.fromDRGB(col, col);
-			col[1] = value;
-			//int c = (int) MathUtil.mapValue(value, 0, 1, 0, 255);
-			//c = c | (c << 8) | (c << 16);
-			ColorConversion.HSL.toDRGB(col, col);
-			int c = ColorConversion.RGB.toRGB(col);
-			return new Color(c);
+		public static BufferedImage assertSize(BufferedImage bi, int width, int height) {
+			if (bi == null || bi.getWidth() != width || bi.getHeight() != height || bi.getType() != BufferedImage.TYPE_INT_RGB)
+				bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			return bi;
+		}
+		
+		public static  BufferedImage genImg(BufferedImage bi, Matrix m, int baseColor) {
+			if (m == null || m.getSizeX() <= 0 || m.getSizeY() <= 0)
+				return null;
+			bi = assertSize(bi, m.getSizeX(), m.getSizeY());
+			double col1[] = new double[3];
+			double col2[] = new double[3];
+			ColorConversion.RGB.fromRGB(baseColor, col1);
+			ColorConversion.HSL.fromDRGB(col1, col1);
+
+			for (int i = m.getSizeX() - 1; i >= 0; i--) {
+				for (int j = m.getSizeY() - 1; j >= 0; j--) {
+					col1[1] = m.getItem(i, j); //MathUtil.mapValue(m.getItem(i, j), 0d, 1d, 0d, 1d);
+					ColorConversion.HSL.toDRGB(col1, col2);
+					int rgb = ColorConversion.RGB.toRGB(col2);
+					bi.setRGB(i, j, rgb);
+				}
+			}
+			return bi;
 		}
 		
 		public synchronized void paint(Graphics g) {
-			if (m.getSizeX() == 0 || m.getSizeY() == 0) {
-				g.fillRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
-				return;
+			BufferedImage img = this.img;
+			if (img == null) {
+				g.setColor(new Color(baseColor));
+				g.fillRect(0, 0, getWidth(), getHeight());
+			} else {
+				g.drawImage(img, 0, 0, getWidth(), getHeight(), null);
 			}
-			double dX = ((double) this.getWidth()) / m.getSizeX(); 
-			double dY = ((double) this.getHeight()) / m.getSizeY();
-
-			for (int i = 0; i < m.getSizeX(); i++)
-				for (int j = 0; j < m.getSizeY(); j++) {
-					g.setColor(getColor(m.getItem(i, j)));
-					g.fillRect(
-							(int) (i * dX), 
-							(int) (j * dY), 
-							(int)((i + 1) * dX) - (int)(i * dX), 
-							(int)((j + 1) * dY) - (int)(j * dY));
-				}
 		}
 	}
 	
 	public void doIt(String[] args) throws Exception {
 		UIManager.setLookAndFeel(new com.sun.java.swing.plaf.gtk.GTKLookAndFeel());
 		JFrame frame = new JFrame();
+		frame.setBackground(Color.RED); // maximze flicker. try to find a solution
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		SwingMatrix sm = new SwingMatrix();
