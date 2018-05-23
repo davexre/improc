e88768334559f8fb1d2b30593e15337d9919bb00
Slavian Petrov <spetrov@ifao.net>
2018-05-23 13:55:59 +0300

    .

--- work/src/main/java/com/slavi/util/ui/BitMatrix.java
+++ work/src/main/java/com/slavi/util/ui/BitMatrix.java
@@ -6,7 +6,9 @@ import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 
+import com.slavi.math.MathUtil;
 import com.slavi.math.matrix.Matrix;
+import com.slavi.util.ColorConversion;
 import com.slavi.util.MatrixUtil;
 
 public class BitMatrix extends JComponent {
@@ -62,13 +64,42 @@ public class BitMatrix extends JComponent {
 		g.drawLine(0, toY, toX, 0);
 	}
 
+	public BufferedImage toBitImage(Matrix m, double threshold, BufferedImage dest) {
+		BufferedImage result = dest;
+		if (result == null ||
+			result.getWidth() != m.getSizeX() ||
+			result.getHeight() != m.getSizeY() ||
+			result.getType() != BufferedImage.TYPE_INT_RGB)
+			result = new BufferedImage(m.getSizeX(), m.getSizeY(), BufferedImage.TYPE_INT_RGB);
+		int fc = this.getForeground().getRGB();
+		int bc = this.getBackground().getRGB();
+		for (int i = 0; i < m.getSizeX(); i++)
+			for (int j = 0; j < m.getSizeY(); j++) {
+				result.setRGB(i, j, m.getItem(i, j) > threshold ? fc : bc);
+			}
+		return result;
+	}
+
 	public void paint(Graphics g) {
 		if (matrix == null) {
 			g.setColor(Color.BLACK);
 			g.fillRect(0, 0, getWidth(), getHeight());
 			return;
 		}
-		img = MatrixUtil.toImage(matrix, unCheckedValue, checkedValue, 0, img);
+		if (usingGradientColors) {
+			img = MatrixUtil.toImage(matrix, unCheckedValue, checkedValue, 0, img);
+			g.drawImage(img, 0, 0, getWidth(), getHeight(), null);
+			return;
+		}
+		double mmin = matrix.min();
+		double mmax = matrix.max();
+		double middle = (mmin + mmax) / 2;
+		double delta = mmax - mmin;
+		if (delta == 0) {
+			drawX(g);
+			return;
+		}
+		img = toBitImage(matrix, middle, img);
 		g.drawImage(img, 0, 0, getWidth(), getHeight(), null);
 	}
 }
