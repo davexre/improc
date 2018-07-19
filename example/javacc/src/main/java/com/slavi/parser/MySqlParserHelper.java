908747b07a01a1dc7136fd7d41864db5c114fb82
Slavian Petrov <slavianp@gmail.com>
2018-07-19 22:29:54 +0300

    .

--- example/javacc/src/main/java/com/slavi/parser/MySqlParserHelper.java
+++ example/javacc/src/main/java/com/slavi/parser/MySqlParserHelper.java
@@ -3,36 +3,47 @@ package com.slavi.parser;
 public class MySqlParserHelper {
 	public StringBuilder cmd = new StringBuilder();
 
-
+	int line;
 	boolean needsSlash = false;
 	boolean slashBlocked = false;
 
-	public void add2(String str) {
+	public void add2(Token token) {
 		if (!slashBlocked)
 			needsSlash = true;
-		System.out.println(">" + str + "<");
-		doAdd(str);
+		//System.out.println(">" + str + "<");
+		doAdd(token);
 	}
 
-	public void add(String str) {
+	public void add(Token token) {
 		slashBlocked = true;
-		System.out.println("|" + str + "|");
-		doAdd(str);
+		//System.out.println("|" + str + "|");
+		doAdd(token);
 	}
 
-	void execCmd() {
-		System.out.println(">> " + cmd);
+	public void addComment(Token token) {
+		if (cmd.length() > 0)
+			doAdd(token);
+	}
+
+	void execCmd(Token token) {
+		if (cmd.length() > 0) {
+			System.out.println(">> " + line + ": " + cmd);
+			System.out.println();
+		}
 		cmd.setLength(0);
 		needsSlash = false;
 		slashBlocked = false;
+		line = 0;
 	}
 
-	void doAdd(String str) {
-		if ("/".equals(str) ||
-			((!needsSlash) && ";".equals(str))) {
-			execCmd();
+	void doAdd(Token token) {
+		if ("/".equals(token.image) ||
+			((!needsSlash) && ";".equals(token.image))) {
+			execCmd(token);
 		} else {
-			cmd.append(str).append(" ");
+			if (cmd.length() == 0)
+				line = token.beginLine;
+			cmd.append(token);
 		}
 	}
 }
