9615d67e30aae6a151f007a1c5e04889328a2e5c
Slavyan Petrov <spetrov@ifao.net>
2018-08-09 12:00:02 +0300

    .

--- example/example.logging/src/main/java/com/slavi/example/logging/SLF4J.java
+++ example/example.logging/src/main/java/com/slavi/example/logging/SLF4J.java
@@ -3,17 +3,28 @@ package com.slavi.example.logging;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.classic.util.ContextSelectorStaticBinder;
+import ch.qos.logback.core.joran.spi.JoranException;
+
 public class SLF4J {
 
 	Logger log = LoggerFactory.getLogger(getClass());
 	Logger log2 = LoggerFactory.getLogger("dummyLogger");
-	
-	void doSomething() {
-		log.debug("This is a debug message");
-		log2.info("This is a Dummy debug message");
+
+	void doSomething() throws JoranException {
+		log.debug("Logger 1 message 1");
+		log2.info("Logger 2 message 1");
+		LoggerContext lc = ContextSelectorStaticBinder.getSingleton().getContextSelector().getDefaultLoggerContext();
+		lc.reset();
+		new ContextInitializer(lc).configureByResource(SLF4J.class.getResource("/logback_alternative.xml"));
+
+		log.debug("Logger 1 message 2");
+		log2.info("Logger 2 message 2");
 	}
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 		new SLF4J().doSomething();
 	}
 }
