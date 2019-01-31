bdb94704651cd12505b0e0cb4c36d6428db6a6ab
Slavian Petrov <slavianp@gmail.com>
2019-01-31 22:21:00 +0200

    .

---
+++ work/src/main/java/com/slavi/reporting/freemarker/HtmlReportTest.java
@@ -0,0 +1,136 @@
+package com.slavi.reporting.freemarker;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.derby.jdbc.EmbeddedDataSource;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import com.slavi.dbutil.MyDbScriptRunner;
+import com.slavi.derbi.Derby;
+import com.slavi.reporting.velocity.ResultSetToIteratorList;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class HtmlReportTest {
+
+	public static class RS {
+		List cols;
+		Iterator rows;
+
+		public List getCols() {
+			return cols;
+		}
+
+		public Iterator getRows() {
+			return rows;
+		}
+
+		public RS(ResultSetToIteratorList rs) {
+			cols = rs.getColumnNames();
+			rows = rs;
+		}
+	}
+
+	public static class Utils {
+		Connection conn;
+
+		public Utils(Connection conn) {
+			this.conn = conn;
+		}
+
+		public String esc(String str) {
+			return StringEscapeUtils.escapeHtml4(str);
+		}
+
+		public RS sqlAsList(String sql) throws SQLException {
+			return new RS(new ResultSetToIteratorList(conn.createStatement().executeQuery(sql)));
+		}
+/*
+		public Map sqlAsList(String sql) throws SQLException {
+			Map r = new HashMap();
+			ResultSetToIteratorList i = new ResultSetToIteratorList(conn.createStatement().executeQuery(sql));
+			r.put("rows", i);
+			r.put("cols", i.getColumnNames());
+			return r;
+		}*/
+	}
+
+	public static DataSource generateHrDb() throws Exception {
+		EmbeddedDataSource ds = new EmbeddedDataSource();
+		ds.setDatabaseName("memory:hr");
+		ds.setCreateDatabase("create");
+		try (Connection conn = ds.getConnection()) {
+			MyDbScriptRunner sr = new MyDbScriptRunner(conn);
+			sr.process(Derby.class.getResourceAsStream("Derby_HR_schema.sql.txt"));
+		}
+		return ds;
+	}
+
+	DataSource ds;
+
+	void doIt() throws Exception {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		ds = generateHrDb();
+
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+		//cfg.setClassForTemplateLoading(getClass(), "");
+		cfg.setClassLoaderForTemplateLoading(getClass().getClassLoader(), "");
+		Template template = cfg.getTemplate("/com/slavi/reporting/freemarker/RecordsetToHtml.ftlh");
+
+		String fou = "target/report";
+		try (
+				Connection conn = ds.getConnection();
+			) {
+			Writer html = new StringWriter();
+
+			Map<String, Object> ctx = new HashMap<>();
+			ctx.put("su", StringUtils.class);
+			ctx.put("date", df.format(new Date()));
+			ctx.put("title", "HTML report test via Apache Freemaker example");
+			ctx.put("items", Arrays.asList("One", "Two", "Three").iterator());
+			ctx.put("seu", StringEscapeUtils.class);
+			ctx.put("utl", new Utils(conn));
+			ctx.put("cssFile", "/resources/bootstrap/4.1.3/css/bootstrap.css");
+
+			template.process(ctx, html);
+
+			System.out.println(html.toString());
+			Writer fhtml = new FileWriter(fou + ".html");
+			fhtml.write(html.toString());
+			fhtml.close();
+
+			// Html to pdf
+			ITextRenderer renderer = new ITextRenderer();
+			renderer.setDocumentFromString(html.toString());
+			renderer.layout();
+			OutputStream pdf = new FileOutputStream(fou + ".pdf");
+			renderer.createPDF(pdf, true);
+			pdf.close();
+		}
+		System.out.println(new File(fou + ".html").getAbsolutePath());
+	}
+
+	public static void main(String[] args) throws Exception {
+		new HtmlReportTest().doIt();
+		System.out.println("Done.");
+	}
+}
