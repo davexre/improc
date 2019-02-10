91a87540c626ecd576cc04a59adea9a1d7a7c001
Slavian Petrov <slavianp@gmail.com>
2019-02-10 20:36:59 +0200

    .

--- work/src/main/java/com/test/java/BeanUtilsExample.java
+++ work/src/main/java/com/test/java/BeanUtilsExample.java
@@ -1,8 +1,15 @@
 package com.test.java;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class BeanUtilsExample {
 
@@ -10,13 +17,31 @@ public class BeanUtilsExample {
 		Map root = new HashMap();
 
 		Map a = new HashMap();
-		int arr[] = new int[] { 3, 5, 7 };
+		int arr[] = new int[] { 3, 5, 7, 8 };
 		a.put("b", arr);
-		
+
 		root.put("A", a);
-		Object o = BeanUtils.getProperty(root, "(Z)(b).[1]");
-		System.out.println(o);
-		System.out.println(o.getClass());
+		root.put("Z", arr);
+
+		Velocity.init();
+		VelocityContext velocityContext = new VelocityContext(root);
+		StringWriter content = new StringWriter();
+		Velocity.evaluate(velocityContext, content, "", "$A.b[1]");
+		content.append('\n');
+		Velocity.evaluate(velocityContext, content, "", "${A.b[1]}");
+		System.out.println(content);
+
+		System.out.println(BeanUtils.getProperty(root, "A.b.[1]"));
+		System.out.println(BeanUtils.getProperty(root, "A.(b)[2]"));
+		System.out.println(BeanUtils.getProperty(root, "(A)(b).[1]"));
+		System.out.println(BeanUtils.getProperty(root, "(A)(b)[1]"));
+
+		ExpressionParser parser = new SpelExpressionParser();
+		System.out.println(parser.parseExpression("[A][b][1]").getValue(root));
+		EvaluationContext context = new StandardEvaluationContext();
+		context.setVariable("A", a);
+		System.out.println(parser.parseExpression("#A[b][1]").getValue(context));
+		System.out.println(parser.parseExpression("#A.get('b')[1]").getValue(context));
 	}
 
 	public static void main(String[] args) throws Exception {
