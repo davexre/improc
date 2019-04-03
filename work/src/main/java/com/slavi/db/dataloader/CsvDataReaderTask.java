e1052afae95052915127362416cdf7f25f395aaf
Slavian Petrov <slavianp@gmail.com>
2019-04-03 23:41:59 +0300

    .

--- work/src/main/java/com/slavi/db/dataloader/CsvDataReaderTask.java
+++ work/src/main/java/com/slavi/db/dataloader/CsvDataReaderTask.java
@@ -1,9 +1,11 @@
 package com.slavi.db.dataloader;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
+import org.apache.commons.csv.CSVParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +17,7 @@ public class CsvDataReaderTask implements Callable<Void> {
 
 	Config cfg;
 	CloseableBlockingQueue<Map<String, Object>> rows;
+	CSVParser parser = null;
 
 	public CsvDataReaderTask(Config cfg, CloseableBlockingQueue<Map<String, Object>> rows, InputStream is) {
 		this.cfg = cfg;
@@ -24,6 +27,15 @@ public class CsvDataReaderTask implements Callable<Void> {
 
 	@Override
 	public Void call() throws Exception {
+		try (AutoCloseable dummy = rows) {
+			Map<String, Object> cur = new HashMap<>();
+			var iter = parser.iterator();
+			while (iter.hasNext()) {
+				var rec = iter.next();
+				//parser.getHeaderMap();
+				rows.put(cur);
+			}
+		}
 		return null;
 	}
 }
